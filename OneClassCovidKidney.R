08995fbfef2de62d9c674b3879ca86f22552cd9b
hugoas <hugo.souza82@gmail.com>
2020-05-13 16:57:36 -0300

    Fianl version

--- OneClassCovidKidney.R
+++ OneClassCovidKidney.R
@@ -8,29 +8,21 @@ library(data.table)
 ds = read.csv('C:/Users/hugos/Desktop/FS Dataset/Health/kidney-disease-dataset/kidney_disease.csv', 
               header = TRUE)
 
-mycols <- c("id","bp","sg","al","su")
+mycols <- c("id","bp","sg","al","su", "bgr","bu","sc", "sod","pot","hemo","pcv","wc","rc")
 
 #Convert to numeric
 setDT(ds)[, (mycols) := lapply(.SD, as.numeric), .SDcols = mycols]
 
 #Convert classification to logical
-data <- ds[,.(bp,sg,al,su,classification = ds$classification == "ckd")]
+data <- ds[,.(bp,sg,al,su,bgr,bu,sc,sod,pot,hemo,pcv,wc,rc,classification = ds$classification == "ckd")]
 
 dataclean <- na.omit(data)
 
-inTrain<-createDataPartition(1:nrow(dataclean),p=0.6,list=FALSE)
+#Separating train and test
+inTrain<-createDataPartition(1:nrow(dataclean),p=0.7,list=FALSE)
 train<- dataclean[inTrain]
 test <- dataclean[-inTrain]
 
-
-#trainpredictors<-trainPositive[inTrain,1:4]
-#trainLabels<-trainPositive[inTrain,6]
-
-#testPositive<-trainPositive[-inTrain,]
-#testPosNeg<-rbind(testPositive,testnegative)
-
-#testpredictors<-testPosNeg[,1:4]
-#testLabels<-testPosNeg[,6]
 
 svm.model<-svm(classification ~ bp + sg + al + su, data = train,
                type='one-classification',
@@ -49,19 +41,7 @@ confTest <- table(Predicted=svm.predtest,
 
 confusionMatrix(confTest,positive='TRUE')
 
-
-svm.predtrain<-predict(svm.model,trainpredictors)
-svm.predtest<-predict(svm.model,testpredictors)
-
-# confusionMatrixTable<-table(Predicted=svm.pred,Reference=testLabels)
-# confusionMatrix(confusionMatrixTable,positive='TRUE')
-
-confTrain <- table(Predicted=svm.predtrain,Reference=trainLabels)
-confTest <- table(Predicted=svm.predtest,Reference=testLabels)
-
-confusionMatrix(confTest,positive='TRUE')
-
 print(confTrain)
 print(confTest)
 
-#grid
+
